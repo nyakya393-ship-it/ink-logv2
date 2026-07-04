4d02ed7bf0736660e1001eb3caa695d664dbf951
Sio <nyakya393@gmail.com>
2026-07-04 12:47:52 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -31,3 +31,51 @@ function showToast(message){
     },2000);
 
 }
+/* =========================
+   PAGE CHANGE
+========================= */
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const buttons=document.querySelectorAll(".nav-btn");
+
+    const pages=document.querySelectorAll(".page");
+
+    const title=document.querySelector(".topbar h2");
+
+    const titles=[
+        "ホーム",
+        "試合登録",
+        "戦績",
+        "分析",
+        "バッジ",
+        "設定"
+    ];
+
+    buttons.forEach((button,index)=>{
+
+        button.addEventListener("click",()=>{
+
+            buttons.forEach(btn=>
+                btn.classList.remove("active")
+            );
+
+            button.classList.add("active");
+
+            pages.forEach(page=>
+                page.classList.remove("active")
+            );
+
+            if(index>0){
+
+                pages[index-1].classList.add("active");
+
+            }
+
+            title.textContent=titles[index];
+
+        });
+
+    });
+
+});
