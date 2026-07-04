c7d461194c84f7cf90ab1e28dc326576d8dc4181
Sio <nyakya393@gmail.com>
2026-07-04 12:25:42 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,33 @@
+/* =========================
+   UI
+========================= */
 
+function showToast(message){
+
+    const toast = document.createElement("div");
+
+    toast.className = "toast";
+
+    toast.textContent = message;
+
+    document.body.appendChild(toast);
+
+    requestAnimationFrame(() => {
+
+        toast.classList.add("show");
+
+    });
+
+    setTimeout(() => {
+
+        toast.classList.remove("show");
+
+        setTimeout(() => {
+
+            toast.remove();
+
+        },300);
+
+    },2000);
+
+}
