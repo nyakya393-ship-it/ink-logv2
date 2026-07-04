9b9c44c6187b55a8d95ce53d03c65c8248b6a23d
Sio <nyakya393@gmail.com>
2026-07-04 12:42:12 +0900

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,1 +1,83 @@
+/* =========================
+   HISTORY
+========================= */
 
+function renderBattleList(){
+
+    const battleList=document.getElementById("battleList");
+
+    if(!battleList) return;
+
+    const battles=loadBattles();
+
+    battleList.innerHTML="";
+
+    if(battles.length===0){
+
+        battleList.innerHTML=`
+        <div class="empty-card">
+            まだ試合が登録されていません
+        </div>
+        `;
+
+        return;
+
+    }
+
+    battles.forEach((battle,index)=>{
+
+        const card=document.createElement("div");
+
+        card.className="battle-card";
+
+        card.innerHTML=`
+
+<div class="battle-top">
+
+<span class="battle-result ${battle.result.includes("勝")?"win":"lose"}">
+
+${battle.result}
+
+</span>
+
+<span>
+
+${battle.rule}
+
+</span>
+
+</div>
+
+<div class="battle-stage">
+
+${battle.stage}
+
+</div>
+
+<div class="battle-weapon">
+
+${battle.weapon}
+
+</div>
+
+<div class="battle-score">
+
+${battle.kill}/${battle.assist}/${battle.death}
+
+&nbsp;&nbsp;
+
+SP ${battle.special}
+
+&nbsp;&nbsp;
+
+${battle.paint}p
+
+</div>
+
+`;
+
+        battleList.appendChild(card);
+
+    });
+
+}
